6346720ee665757fdfc7708a9e9aecb1184c07f5
Angelo L. Salenga <alsalenga@asyadesign.local>
2026-04-08 14:20:29 +0800

    new updated for today

--- js/script.js
+++ js/script.js
@@ -1,15 +1,24 @@
-const faders = document.querySelectorAll(".fade-in");
-
-window.addEventListener("scroll", () => {
-  faders.forEach((el) => {
-    const top = el.getBoundingClientRect().top;
-    if (top < window.innerHeight - 100) {
-      el.classList.add("show");
-    }
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: true,
+    items: 4,
   });
 });
 
-const eventDate = new Date("April 20, 2026 11:00:00").getTime();
+// const faders = document.querySelectorAll(".fade-in");
+// window.addEventListener("scroll", () => {
+//   faders.forEach((el) => {
+//     const top = el.getBoundingClientRect().top;
+//     if (top < window.innerHeight - 100) {
+//       el.classList.add("show");
+//     }
+//   });
+// });
+
+const eventDate = new Date("June 14, 2026 11:00:00").getTime();
 
 setInterval(() => {
   const now = new Date().getTime();
@@ -25,3 +34,10 @@ setInterval(() => {
     (diff / (1000 * 60)) % 60,
   );
 }, 1000);
+
+var markers = [L.marker([14.91184, 120.56421]), L.marker([14.92508, 120.5299])];
+
+var group = L.featureGroup(markers);
+map.fitBounds(group.getBounds(), {
+  padding: window.innerWidth <= 768 ? [20, 20] : [50, 50],
+});
